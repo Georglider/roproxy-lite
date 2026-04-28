d64963900911a7f642a755fc4b2eb55b11e4a2fa
George <2323georg@gmail.com>
2026-04-29 01:44:15 +0300

    feat: Dockerfile feat: Default port feat: Allow disabling PROXYKEY

--- main.go
+++ main.go
@@ -2,39 +2,47 @@ package main
 
 import (
 	"log"
-	"time"
 	"os"
-	"github.com/valyala/fasthttp"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/valyala/fasthttp"
 )
 
 var timeout, _ = strconv.Atoi(os.Getenv("TIMEOUT"))
 var retries, _ = strconv.Atoi(os.Getenv("RETRIES"))
-var port = os.Getenv("PORT")
+var port, hasPort = os.LookupEnv("PORT")
 
 var client *fasthttp.Client
 
 func main() {
 	h := requestHandler
-	
+
 	client = &fasthttp.Client{
-		ReadTimeout: time.Duration(timeout) * time.Second,
+		ReadTimeout:         time.Duration(timeout) * time.Second,
 		MaxIdleConnDuration: 60 * time.Second,
 	}
 
-	if err := fasthttp.ListenAndServe(":" + port, h); err != nil {
+	if !hasPort {
+		port = "8080"
+	}
+	if err := fasthttp.ListenAndServe(":"+port, h); err != nil {
 		log.Fatalf("Error in ListenAndServe: %s", err)
 	}
 }
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
 	val, ok := os.LookupEnv("KEY")
+	_, disableProxyKey := os.LookupEnv("DISABLE_PROXYKEY")
 
 	if ok && string(ctx.Request.Header.Peek("PROXYKEY")) != val {
-		ctx.SetStatusCode(407)
-		ctx.SetBody([]byte("Missing or invalid PROXYKEY header."))
-		return
+		if !disableProxyKey {
+			ctx.SetStatusCode(407)
+			ctx.SetBody([]byte("Missing or invalid PROXYKEY header."))
+			return
+		}
+		ctx.Request.Header.Set("x-api-key", val)
 	}
 
 	if len(strings.SplitN(string(ctx.Request.Header.RequestURI())[1:], "/", 2)) < 2 {
@@ -43,19 +51,19 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 		return
 	}
 
-	response := makeRequest(ctx, 1)
+	response := makeRequest(ctx, val, 1)
 
 	defer fasthttp.ReleaseResponse(response)
 
 	body := response.Body()
 	ctx.SetBody(body)
 	ctx.SetStatusCode(response.StatusCode())
-	response.Header.VisitAll(func (key, value []byte) {
+	response.Header.VisitAll(func(key, value []byte) {
 		ctx.Response.Header.Set(string(key), string(value))
 	})
 }
 
-func makeRequest(ctx *fasthttp.RequestCtx, attempt int) *fasthttp.Response {
+func makeRequest(ctx *fasthttp.RequestCtx, key string, attempt int) *fasthttp.Response {
 	if attempt > retries {
 		resp := fasthttp.AcquireResponse()
 		resp.SetBody([]byte("Proxy failed to connect. Please try again."))
@@ -70,7 +78,7 @@ func makeRequest(ctx *fasthttp.RequestCtx, attempt int) *fasthttp.Response {
 	url := strings.SplitN(string(ctx.Request.Header.RequestURI())[1:], "/", 2)
 	req.SetRequestURI("https://" + url[0] + ".roblox.com/" + url[1])
 	req.SetBody(ctx.Request.Body())
-	ctx.Request.Header.VisitAll(func (key, value []byte) {
+	ctx.Request.Header.VisitAll(func(key, value []byte) {
 		req.Header.Set(string(key), string(value))
 	})
 	req.Header.Set("User-Agent", "RoProxy")
@@ -79,10 +87,10 @@ func makeRequest(ctx *fasthttp.RequestCtx, attempt int) *fasthttp.Response {
 
 	err := client.Do(req, resp)
 
-    if err != nil {
+	if err != nil {
 		fasthttp.ReleaseResponse(resp)
-        return makeRequest(ctx, attempt + 1)
-    } else {
-		return resp
+		return makeRequest(ctx, key, attempt+1)
 	}
+
+	return resp
 }
